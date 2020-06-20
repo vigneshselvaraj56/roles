5f4c0dca7978f54a306c0a4cba55bfcc35cb4a53
root <root@puppetmaster.devops.com>
2020-06-20 08:06:22 +0000

    roles

---
+++ manifests/rhel7/base.pp
@@ -0,0 +1,3 @@
+class roles::rhel7::base {
+  include profiles::os::linux::base_rhel7
+}
---
+++ manifests/rhel7/web_tomcat.pp
@@ -0,0 +1,5 @@
+class roles::rhel7::web_tomcat (
+  include profiles::os::linux::base_rhel7
+  include profiles::applications::web_tomcat
+}
+
