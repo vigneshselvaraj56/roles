700981f8a9db0bcb48f8c872118dd8dfeb3d5555
vigneshselvaraj56 <66637671+vigneshselvaraj56@users.noreply.github.com>
2020-06-23 22:17:19 +0530

    Update puppet_role.rb

--- lib/facter/puppet_role.rb
+++ lib/facter/puppet_role.rb
@@ -1,5 +1,5 @@
 Facter.add('role') do
   setcode do
-    Facter::core::Execution.execute ('/usr/bin/cat  /opt/puppetlabs/puppet/cache/state/classes.txt | grep -i roles | cut -c8-')
+    Facter::core::Execution.execute('/usr/bin/cat /opt/puppetlabs/puppet/cache/state/classes.txt | grep -i roles | cut -c8-')
   end
 end
